488699c3c9e009db061ceb8aa68f3a20238fc2b7
Waygou <bruno.falcao@waygou.com>
2018-11-25 18:59:10 +0100

    Update SvgIcon.php

--- src/Models/SvgIcon.php
+++ src/Models/SvgIcon.php
@@ -3,8 +3,11 @@
 namespace Waygou\XheetahUtils\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Hyn\Tenancy\Traits\UsesSystemConnection;
 
 class SvgIcon extends Model
 {
+    use UsesSystemConnection;
+
     protected $guarded = [];
 }
